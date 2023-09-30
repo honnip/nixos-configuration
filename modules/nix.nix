aabaec6c0072da6293193ceeb49244c49913cdee
honnip <me@honnip.page>
2023-09-30 11:04:26 +0900

    NixOS settings

--- modules/nix.nix
+++ modules/nix.nix
@@ -1,6 +1,18 @@
 {
   nix = {
-    settings = { trusted-users = [ "root" "@wheel" ]; };
-    extraOptions = "experimental-features = nix-command flakes ";
+    settings = {
+      trusted-users = [ "root" "@wheel" ];
+      auto-optimise-store = true;
+    };
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 14d";
+    };
+    extraOptions = "
+      experimental-features = nix-command flakes
+      keep-outputs = true
+      keep-derivations = true
+    ";
   };
 }
