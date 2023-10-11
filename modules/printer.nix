ceb28e58a73eb60d34588bc695018dec35ca2f60
honnip <me@honnip.page>
2023-10-11 21:00:29 +0900

    make printer works

--- modules/printer.nix
+++ modules/printer.nix
@@ -1,6 +1,16 @@
-{ pkgs, ... }: {
+{ pkgs, config, ... }: {
   services.printing = {
     enable = true;
-    drivers = with pkgs; [ canon-cups-ufr2 ];
   };
+  services.avahi = {
+    enable = true;
+    nssmdns = false; # use mdns4
+    openFirewall = true;
+  };
+
+  system.nssModules = with pkgs.lib; optional (!config.services.avahi.nssmdns) pkgs.nssmdns;
+    system.nssDatabases.hosts = with pkgs.lib; optionals (!config.services.avahi.nssmdns) (mkMerge [
+      (mkOrder 900 [ "mdns4_minimal [NOTFOUND=return]" ]) # must be before resolve
+      (mkOrder 1501 [ "mdns4" ]) # 1501 to ensure it's after dns
+    ]);
 }
