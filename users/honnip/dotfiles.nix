ddf1e3c430fb53d5fe4643751cbbf9d3f9bce090
honnip <me@honnip.page>
2023-10-14 23:50:04 +0900

    `.gitconfig` to `.config/git/config`

--- users/honnip/dotfiles.nix
+++ users/honnip/dotfiles.nix
@@ -1,6 +1,5 @@
 {
   home.file = {
-    ".gitconfig".source = ./dotfiles/.gitconfig;
     ".cargo/config.toml".source = ./dotfiles/.cargo/config.toml;
     ".ssh/config".source = ./dotfiles/.ssh/config;
     ".config" = {
