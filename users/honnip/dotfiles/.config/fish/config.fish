7767f5c859623c379852b7b8cb9dcf31b5ff183b
honnip <me@honnip.page>
2023-12-01 10:48:14 +0900

    dotfiles update

--- users/honnip/dotfiles/.config/fish/config.fish
+++
@@ -1,9 +0,0 @@
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-end
-
-alias nvmetmp='sudo nvme smart-log /dev/nvme0n1 | rg ^temperature'
-
-mcfly init fish | source
-starship init fish | source
-direnv hook fish | source
