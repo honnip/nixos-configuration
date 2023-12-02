00c0782a3a1d1fb0896c12dc2691764e5c251cca
honnip <me@honnip.page>
2023-12-02 12:22:35 +0900

    update mime

--- users/honnip/mimeApps.nix
+++ users/honnip/mimeApps.nix
@@ -2,6 +2,7 @@
   xdg.mimeApps = {
     enable = true;
     defaultApplications = {
+      # firefox
       "x-scheme-handler/http" = "firefox.desktop";
       "x-scheme-handler/https" = "firefox.desktop";
       "x-scheme-handler/chrome" = "firefox.desktop";
@@ -17,15 +18,23 @@
       "text/csv" = "org.gnome.TextEditor.desktop";
       "text/markdown" = "org.gnome.TextEditor.desktop";
       "text/xml" = "org.gnome.TextEditor.desktop";
-
       "application/pdf" = "org.gnome.Evince.desktop";
 
+      "application/zip" = "org.gnome.FileRoller.desktop";
+
+      # video & audio
       "audio/mp4" = "org.gnome.Totem.desktop";
       "video/mp4" = "org.gnome.Totem.desktop";
+      "video/webm" = "org.gnome.Totem.desktop";
+      
 
-      "image/jpeg" = "org.gnome.eog.desktop";
-      "image/png" = "org.gnome.eog.desktop";
-      "image/svg+xml" = "org.gnome.eog.desktop";
+      # image
+      "image/jpeg" = "org.gnome.Loupe.desktop";
+      "image/png" = "org.gnome.Loupe.desktop";
+      "image/avif" = "org.gnome.Loupe.desktop";
+      "image/gif" = "org.gnome.Loupe.desktop";
+      "image/webp" = "org.gnome.Loupe.desktop";
+      "image/svg+xml" = "org.gnome.Loupe.desktop";
 
       "x-scheme-handler/mailto" = "userapp-Thunderbird-TT1RA2.desktop";
       "x-scheme-handler/tg" = "org.telegram.desktop.desktop";
